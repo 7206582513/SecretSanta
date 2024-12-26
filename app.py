012ca7578f0c4585969af5eb5744ac6d995ac160
7206582513 <rohitmakani564@gmail.com>
2024-12-26 19:36:43 +0530

    port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import random
+import os
 
 app = Flask(__name__)
 
@@ -32,5 +33,6 @@ def assign():
     return jsonify(pairings)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
 
